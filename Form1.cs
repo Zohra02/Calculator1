b03607dedb87aa693cd5a507f35b92b9462f9aee
Your Name <you@example.com>
2022-10-15 18:59:59 +0600

    first commit

---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace my1stcalculator
+{
+    public partial class Form1 : Form
+    {
+
+        int result;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button2.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button3.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button4.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button5.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button6.Text;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button7.Text;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button8.Text;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button9.Text;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button10.Text;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            result = result + int.Parse(textBox1.Text);
+            textBox1.Clear();
+            
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            result = result + int.Parse(textBox1.Text);
+            textBox1.Text = result.ToString();
+            result = 0;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+    }
+}
